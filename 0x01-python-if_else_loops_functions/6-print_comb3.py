400a283e6de659fc4f1cee236f9b249a33d194fd
Ammar-Khaled <ammar.khaled.github@gmail.com>
2023-05-09 11:31:05 +0000

    fix bugs

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-for i in range(9): # 0 1 2 3 .. 8
-	j = i + 1
-	while j <= 9:  # i+1 ... 9
-		if i == 8 and j == 9:
-			print('{}{}'.format(i, j))
-		else:
-			print('{}{}'.format(i, j), end=', ')
-		j = j + 1
+for i in range(9):  # 0 1 2 3 .. 8
+    j = i + 1
+    while j <= 9:  # i+1 ... 9
+        if i == 8 and j == 9:
+            print('{}{}'.format(i, j))
+        else:
+            print('{}{}'.format(i, j), end=', ')
+        j = j + 1
--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def uppercase(str):
-	for i in range(len(str)):
-		if ord('a') <= ord(str[i]) <= ord('z'):
-			print(chr(ord(str[i]) - 32), end='')
-		else:
-			print(str[i], end='')
-	print()
+    for i in range(len(str)):
+        if ord('a') <= ord(str[i]) <= ord('z'):
+            print('{:c}'.format(ord(str[i]) - 32), end='')
+        else:
+            print(str[i], end='')
+    print()
