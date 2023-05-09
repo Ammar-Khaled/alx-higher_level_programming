3fd0ace4fac43a6a540e3914daa2bb94c789c44d
Ammar-Khaled <ammar.khaled.github@gmail.com>
2023-05-09 10:54:02 +0000

    fix bugs

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def remove_char_at(str, n):
 
-    # if index is out of range remove nothing   
+    # if index is out of range remove nothing
     if n < 0 or n > len(str) - 1:
         return str
     
@@ -9,5 +9,5 @@ def remove_char_at(str, n):
     str[n] = ''
     ans = ''
     for i in range(len(str)):
-        ans += str[i] 
+        ans += str[i]
     return ans
--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -3,7 +3,7 @@ for i in range(9): # 0 1 2 3 .. 8
 	j = i + 1
 	while j <= 9:  # i+1 ... 9
 		if i == 8 and j == 9:
-			print(f'{i}{j}')
+			print('{}{}'.format(i, j))
 		else:
-			print(f'{i}{j}', end=', ')
+			print('{}{}'.format(i, j), end=', ')
 		j = j + 1
