6d68270c2f7c2e98f9592a1b3c60552f20dadeaf
Ammar-Khaled <ammar.khaled.github@gmail.com>
2023-05-13 14:48:50 +0000

    meet requirements

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,9 +2,8 @@
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         for element in row:
-            print(element, end='')
+            print('{:d}'.format(element), end='')
             if element != row[-1]:
                 print(' ', end='')
         print()
                 
-        
