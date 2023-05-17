7f1078422067db12b022e769793eac64f8772316
Ammar-Khaled <ammar.khaled.github@gmail.com>
2023-05-17 11:32:59 +0000

    fix bugs

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
 def complex_delete(a_dictionary, value):
-    new_dict = dict(a_dictionary)
     to_remove = []
-    for key, v in new_dict.items():
+    for key, v in a_dictionary.items():
         if value == v:
             to_remove.append(key)
 
     for key in to_remove:
-        new_dict.pop(key)
+        a_dictionary.pop(key)
 
-    return new_dict
+    return a_dictionary
--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -3,7 +3,7 @@ def roman_to_int(roman_string):
     """" converts a Roman numeral to an integer. """
     if not roman_string or not isinstance(roman_string, str):
         return 0
-    
+
     roman_numerals = {
         "M": 1000,
         "C": 100,
@@ -16,7 +16,7 @@ def roman_to_int(roman_string):
 
     sum = 0
     for i in range(len(roman_string) - 1):
-        if (roman_numerals[roman_string[i]] < roman_numerals[roman_string[i + 1]]):
+        if roman_numerals[roman_string[i]] < roman_numerals[roman_string[i+1]]:
             sum -= roman_numerals[roman_string[i]]
         else:
             sum += roman_numerals[roman_string[i]]
