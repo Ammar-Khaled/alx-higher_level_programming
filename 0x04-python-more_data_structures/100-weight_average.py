64a13f1ad377f4714246ad11cf4d74783addfa4e
Ammar-Khaled <111376558+Ammar-Khaled@users.noreply.github.com>
2023-05-17 14:26:43 +0300

    Update 100-weight_average.py

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -4,6 +4,8 @@ def weight_average(my_list=[]):
     my_list = [(1, 2), (2, 1), (3, 10), (4, 2)]
 # = ((1 * 2) + (2 * 1) + (3 * 10) + (4 * 2)) / (2 + 1 + 10 + 2)
     '''
+    if not my_list:
+        return 0
     weighted_score_sum = 0.0
     weight_sum = 0.0
     for tuple in my_list:
