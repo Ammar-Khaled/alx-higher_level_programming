7274387133af096085e118f2241fd5a6055b7452
Ammar-Khaled <111376558+Ammar-Khaled@users.noreply.github.com>
2023-05-17 14:14:10 +0300

    Update 7-update_dictionary.py

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
-    updated_dict = dict(a_dictionary)
-    updated_dict.update({key: value})
-    # updated_dict[key] = value also works for new keys
-    return updated_dict
+    a_dictionary.update({key: value})
+    # a_dictionary[key] = value also works for new keys
+    return a_dictionary
