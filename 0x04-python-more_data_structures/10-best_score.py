dfb6561a78aabfe0053d7905544282c19290e5e5
Ammar-Khaled <ammar.khaled.github@gmail.com>
2023-05-17 11:35:18 +0000

    fix style

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -3,5 +3,5 @@ def best_score(a_dictionary):
     if not a_dictionary:
         return (None)
 
-    sorted_tuples = sorted(dict(a_dictionary).items(), key=lambda tuple: tuple[1])
+    sorted_tuples = sorted(dict(a_dictionary).items(), key=lambda t: t[1])
     return sorted_tuples[-1][0]
