915dba25281d86a324b051de8873c42d9ccc1f92
Ammar-Khaled <ammar.khaled.github@gmail.com>
2023-05-29 15:46:47 +0000

    fix

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-def safe_print_integer(value):
+def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
-        return True
     except Exception as e:
         import sys
         sys.stderr.write("Exception: {}\n".format(e))
         return False
+    else:
+        return True
--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def safe_function(fct, *args):
     try:
-        return fct(args)
+        return fct(*args)
     except Exception as e:
         import sys
         sys.stderr.write("Exception: {}\n".format(e))
