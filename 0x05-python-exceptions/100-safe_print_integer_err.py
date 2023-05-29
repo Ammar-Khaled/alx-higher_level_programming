9417831c8eb60db8aed2e37998850b2ccedd7d2f
Ammar-Khaled <ammar.khaled.github@gmail.com>
2023-05-29 15:39:28 +0000

    first trial

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def safe_print_integer(value):
+    try:
+        print("{:d}".format(value))
+        return True
+    except Exception as e:
+        import sys
+        sys.stderr.write("Exception: {}\n", e)
+        return False
+            
