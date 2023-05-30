16aaf9114ee61a014acb02fb3fabe851efd97245
Ammar-Khaled <ammar.khaled.github@gmail.com>
2023-05-30 12:28:36 +0000

    first touch

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+This module is to define a Square object
+"""
+
+
+class Square:
+    """An empty class to define a Square object"""
+    def __init__(self, size):
+        """initialises a new Square instance
+        
+        Args:
+            param1 (int): size of the square
+        """
+        self.__size = size
