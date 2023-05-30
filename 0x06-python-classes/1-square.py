15feb960528281533b7c5dd8ad416011b548228d
Ammar-Khaled <ammar.khaled.github@gmail.com>
2023-05-30 12:29:32 +0000

    fix style

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -8,7 +8,7 @@ class Square:
     """An empty class to define a Square object"""
     def __init__(self, size):
         """initialises a new Square instance
-        
+
         Args:
             param1 (int): size of the square
         """
