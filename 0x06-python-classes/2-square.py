98a57ddb669f5ebb9b2fba0bf59efc9c59c4ea2a
Ammar-Khaled <ammar.khaled.github@gmail.com>
2023-05-30 12:35:01 +0000

    fix style

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+This module is to define a Square object
+"""
+
+
+class Square:
+    """An empty class to define a Square object"""
+    def __init__(self, size=0):
+        """initialises a new Square instance
+
+        Args:
+            param1 (int): size of the square
+        """
+        if (type(size) != int):
+            raise TypeError("size must be an integer")
+        if (size < 0):
+            raise ValueError("size must be >= 0")
+        self.__size = size
