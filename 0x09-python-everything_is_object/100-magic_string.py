b5d3ed7d597fe5bd32c1d3f7ee1ca5da1426ebd4
Ammar-Khaled <ammar.khaled.github@gmail.com>
2023-06-06 16:47:11 +0000

    fix bug

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def magic_string(list=[]):
-    list += ["Best School"]  # with each call, the list object grows in memory
+    list += ["BestSchool"]  # with each call, the list object grows in memory
     return ", ".join(list)
