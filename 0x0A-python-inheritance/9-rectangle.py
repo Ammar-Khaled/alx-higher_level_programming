4a652f3ed35403757e3a545e4bd9eaf29e43dddd
Ammar-Khaled <ammar.khaled.github@gmail.com>
2023-06-12 15:22:19 +0000

    implement 9-rectangle.py

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+""" This module defines a Rectangle class
+"""
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """ class for rectangle shapes
+    """
+
+    def __init__(self, width, height):
+        """ init function
+
+            Args:
+                width: rectangle width
+                height: rectangle height
+
+            Raises:
+                TypeError: if width or height is not be an integer
+                ValueError: if width or height is less than or equal to zero
+          """
+
+        super().integer_validator('width', width)
+        super().integer_validator('height', height)
+
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        """ calculate rectangle area."""
+
+        return self.__width * self.__height
+
+    def __str__(self):
+        """ string info for object."""
+
+        return '[Rectangle] {}/{}'.format(self.__width, self.__height)
