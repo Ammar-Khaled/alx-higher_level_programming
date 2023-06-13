8ae4c6a486a6a8cb615e39deb494e80c8a3e018e
Ammar-Khaled <ammar.khaled.github@gmail.com>
2023-06-13 18:10:36 +0000

    first trial

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+""" This module defines Student class."""
+
+
+class Student:
+    """ represents Student object"""
+
+    def __init__(self, first_name, last_name, age):
+        """ init method"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """ retrieves attrs from the dictionary representation
+            of a Student instance."""
+        full_dict = self.__dict__.copy()
+
+        if type(attrs) == list:
+            for element in attrs:
+                if type(element) != str:
+                    return full_dict
+
+            new_dict = {}
+            for attr in full_dict:
+                if attr in attrs:
+                    new_dict[attr] = full_dict[attr]
+            return new_dict
+
+        return full_dict
+
+    def reload_from_json(self, json):
+        """ replaces all attributes of the Student instance."""
+        for key in json:
+            self.__dict__[key] = json[key]
