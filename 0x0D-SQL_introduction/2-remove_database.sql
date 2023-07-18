d44903859200df660c1425a4decb8a66df28280a
Ammar-Khaled <ammar.khaled.github@gmail.com>
2023-07-18 14:49:15 +0000

    Complete tasks#2-5

---
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -0,0 +1,4 @@
+-- Deletes a database
+
+-- deletes the database `hbtn_0c_0` in my MySQL server if exists.
+DROP DATABASE IF EXISTS hbtn_0c_0;
---
+++ 0x0D-SQL_introduction/3-list_tables.sql
@@ -0,0 +1,4 @@
+-- List tables
+
+-- lists all the tables of a database, passed as argument, in my MySQL server
+SHOW TABLES;
