643679b5a4ee4e4d349e5dc129a8972d6a231262
Ammar-Khaled <ammar.khaled.github@gmail.com>
2023-07-18 16:52:31 +0000

    Complete advanced task#0

---
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -0,0 +1,7 @@
+-- Script that converts hbtn_0c_0 database to UTF8
+-- Query to convert hbtn_0c_0 database to UTF8
+ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+-- by using the hbtn_0c_0 database
+USE hbtn_0c_0;
+-- convert the first table to UTF8
+ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
