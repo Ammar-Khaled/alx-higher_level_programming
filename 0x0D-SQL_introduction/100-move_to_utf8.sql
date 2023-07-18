9e71d3360b8924dc53d47c8b15dfb8469f58ac14
Ammar-Khaled <ammar.khaled.github@gmail.com>
2023-07-18 16:56:13 +0000

    Update

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,7 +1,9 @@
 -- Script that converts hbtn_0c_0 database to UTF8
 -- Query to convert hbtn_0c_0 database to UTF8
 ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
--- by using the hbtn_0c_0 database
+-- by using the `hbtn_0c_0` database
 USE hbtn_0c_0;
--- convert the first table to UTF8
+-- convert the `first` table to UTF8
 ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+-- convert the `name` field to UTF8
+ALTER TABLE first_table MODIFY name VARCHAR(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
