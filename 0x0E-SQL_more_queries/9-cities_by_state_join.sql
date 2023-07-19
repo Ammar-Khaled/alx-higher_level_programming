ca400c57612ca8233f90c2be62923af521d10dc5
Ammar-Khaled <ammar.khaled.github@gmail.com>
2023-07-19 09:23:18 -0700

    Complete task#8,9

---
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,8 @@
+-- a script that lists all cities contained in the database hbtn_0d_usa
+
+-- The Query
+SELECT cities.id AS id, cities.name AS name, states.name AS name
+FROM cities 
+JOIN states
+ON cities.state_id = states.id
+ORDER BY cities.id;
