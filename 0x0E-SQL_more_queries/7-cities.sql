fd94b22a05f14711649c33345eb909ca0e3a50fd
Ammar-Khaled <ammar.khaled.github@gmail.com>
2023-07-19 16:27:49 +0000

    Fix missing column

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -4,10 +4,11 @@
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 
 -- use the usa database
-USE hbtn_0d_usa; 
+USE hbtn_0d_usa;
 
 -- create the table
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities(
 	id INT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY,
-	state_id INT NOT NULL REFERENCES states(id)
+	state_id INT NOT NULL REFERENCES states(id),
+	name VARCHAR(256) NOT NULL,
 );
