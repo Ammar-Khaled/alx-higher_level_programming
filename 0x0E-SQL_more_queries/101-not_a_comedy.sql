75189e78bd668c50671266539c9dcf4f9f4739d0
Ammar-Khaled <ammar.khaled.github@gmail.com>
2023-07-19 20:41:37 +0000

    fix

--- 0x0E-SQL_more_queries/101-not_a_comedy.sql
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -3,14 +3,32 @@ SELECT tv_shows.title AS title  -- Query to get shows that not belong to Comedy 
 FROM tv_shows
 LEFT JOIN
 (
-	SELECT tv_genres.id, tv_genres.name -- Query to get Comdey shows
+	SELECT tv_shows.title -- Query to get Comdey shows
 	FROM tv_genres
 	JOIN tv_show_genres
-	     ON tv_genres.id = tv_show_genres.genre_id
+	ON tv_genres.id = tv_show_genres.genre_id
 	JOIN tv_shows
-	     ON tv_show_genres.show_id = tv_shows.id
+	ON tv_show_genres.show_id = tv_shows.id
 	WHERE tv_genres.name = "Comedy"
-	ORDER BY tv_genres.id
-) Comedy_shows ON Comedy_shows.id = tv_shows.id
-WHERE Comedy_shows.id is NULL
+	ORDER BY tv_shows.title
+) Comedy_shows ON Comedy_shows.title = tv_shows.title
+WHERE Comedy_shows.title is NULL
+ORDER BY tv_shows.title;
+
+
+-- Script that lists shows that don't belong to Comedy genre
+SELECT tv_shows.title -- Query to get shows that are not comedies
+FROM tv_shows
+LEFT JOIN
+(
+	SELECT tv_shows.title -- Query to get Comedy shows
+	FROM tv_shows
+     	JOIN tv_show_genres
+     	     ON tv_show_genres.show_id = tv_shows.id
+     	JOIN tv_genres
+     	     ON tv_genres.id = tv_show_genres.genre_id
+	WHERE tv_genres.name = "Comedy"
+	ORDER BY tv_shows.id
+) comedy_shows ON comedy_shows.title = tv_shows.title
+WHERE comedy_shows.title is NULL
 ORDER BY tv_shows.title;
