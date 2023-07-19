ccd93bab0f3e5a1d44309de4bc6a2572378a451f
Ammar-Khaled <ammar.khaled.github@gmail.com>
2023-07-19 16:29:52 +0000

    Fix: using table-level constraints

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -8,7 +8,8 @@ USE hbtn_0d_usa;
 
 -- create the table
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities(
-	id INT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY,
-	state_id INT NOT NULL REFERENCES states(id),
+	id INT UNIQUE AUTO_INCREMENT NOT NULL,
+	state_id INT NOT NULL,
 	name VARCHAR(256) NOT NULL,
-);
+	PRIMARY KEY (id),
+	FOREIGN KEY (state_id) REFERENCES states(id));
