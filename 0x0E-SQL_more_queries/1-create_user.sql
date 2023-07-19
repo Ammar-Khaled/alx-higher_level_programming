c52b2ba715588964141701988a3462bdb27a058e
Ammar-Khaled-Abd-An_Noor-Ibraheem <111376558+Ammar-Khaled@users.noreply.github.com>
2023-07-19 22:49:04 +0300

    Update 1-create_user.sql

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,7 +1,4 @@
--- a script that creates the MySQL server user user_0d_1 with all privileges.
-
--- create the user
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
--- grant privileges to the user
-GRANT ALL PRIVILEGES ON * . * TO 'user_0d_1'@'localhost'; 
-
+-- Script that creates an user in MySQL server
+-- Query to create the user 'user_0d_1' in MySQL server
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost';
+GRANT ALL PRIVILEGES ON * . * TO 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
