2e8aaf6bbfd6e75df778735478455fe96ba9a020
Ammar-Khaled <ammar.khaled.github@gmail.com>
2023-08-14 08:21:36 -0700

    Complete Task#10

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+function fact (n) {
+  if (n === 1 || n === 0) return (1);
+
+  return (n * fact(n - 1));
+}
+
+const n = +process.argv[2];
+if (isNaN(n)) console.log('1');
+else console.log(fact(n));
