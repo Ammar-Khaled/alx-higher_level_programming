c60467e362a3b1876e596782e0f89ad34eff891b
Ammar-Khaled <ammar.khaled.github@gmail.com>
2023-08-14 08:50:18 -0700

    fix

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -2,8 +2,9 @@
 const n = process.argv.length;
 if (n < 4) console.log('0');
 else {
-  const  myNums = [];
+  const myNums = [];
   for (let i = 2; i < n; i++) myNums.push(+process.argv[i]);
-  myNums.sort();
-  console.log(myNums[n - 4]);
+  // sorting descendingly by value not lexicographicly
+  myNums.sort((a, b) => b - a);
+  console.log(myNums[1]);
 }
