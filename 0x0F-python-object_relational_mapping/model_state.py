869b172ee7eb6ae853f7ecdff88337af6be9b13d
Ammar-Khaled <ammar.khaled.github@gmail.com>
2023-08-17 21:07:17 +0000

    Complete Task#6, 7

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""A python file that contains the class definition of a `State`."""
+
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import MetaData, Column, Integer, String
+
+mymetadata = MetaData()
+Base = declarative_base(metadata=mymetadata)
+
+
+class State(Base):
+    """Define the State that links to the MySQL table `states`."""
+    __tablename__ = 'states'
+    id = Column('id', Integer, autoincrement=True, unique=True, nullable=False, primary_key=True)
+    name = Column('name', String(128), nullable=False)
