24c3f55f9164bf5d10280fb805ac3c84431d7db0
Ammar-Khaled <ammar.khaled.github@gmail.com>
2023-08-31 19:16:21 +0000

    fix style

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,26 +1,28 @@
 #!/usr/bin/python3
 """ Module that finds a peak in a list of unsorted integers """
 
+
 def find_peak(list_of_integers):
     """finds a peak in a list of unsorted integers"""
 
     if not list_of_integers:
         return None
 
-    length = len(list_of_integers)
+    n = len(list_of_integers)
 
-    if length == 0:
+    if n == 0:
         return None
 
-    if length == 1:
+    if n == 1:
         return list_of_integers[0]
 
     start = 0
-    end = length - 1
+    end = n - 1
     ls = list_of_integers
     while start <= end:
-        mid = (start + end) >> 1
-        if (mid == 0 or ls[mid] >= ls[mid - 1]) and (mid == length - 1 or ls[mid] >= ls[mid + 1]):
+        mid = start + (end - start) // 2
+        if (mid == 0 or ls[mid] >= ls[mid - 1]) and\
+           (mid == n - 1 or ls[mid] >= ls[mid + 1]):
             break
         elif mid > 0 and ls[mid] < ls[mid - 1]:
             end = mid - 1
