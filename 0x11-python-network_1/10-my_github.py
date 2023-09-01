746d782016e64b8698763978941a9c5abd264b72
Ammar-Khaled <ammar.khaled.github@gmail.com>
2023-09-01 15:25:27 +0000

    Completed Task#9

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+This module takes your GitHub credentials (username and password),
+and uses the GitHub API to display your id.
+"""
+
+if __name__ == '__main__':
+    import requests
+    from sys import argv
+    import json
+    username = argv[1]
+    password = argv[2]
+    url = 'https://api.github.com/user'
+    headers = {'Authorization': f'Bearer {password}',
+               'Accept': 'application/vnd.github+json',
+               'X-GitHub-Api-Version': '2022-11-28'}
+    res = requests.get(url, headers=headers)
+    res_dict = json.loads(res.text)
+    try:
+        print(res_dict['id'])
+    except:
+        print('None')
