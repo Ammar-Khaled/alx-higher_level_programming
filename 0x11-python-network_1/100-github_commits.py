2352bc1498bd1a0e0c508169ef56096e001219e2
Ammar-Khaled <ammar.khaled.github@gmail.com>
2023-09-01 16:12:58 +0000

    fix

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -7,13 +7,16 @@ and uses the GitHub API to display the last recent 10 commits.
 if __name__ == '__main__':
     import requests
     from sys import argv
-    repo_name = argv[1]
-    owner_name = argv[2]
-    url = f'https://api.github.com/repos/{owner_name}/{repo_name}/commits'
-    headers = {'Accept': 'application/vnd.github+json',
-               'X-GitHub-Api-Version': '2022-11-28'}
-    res = requests.get(url, headers=headers)
-    commit_list = res.json()
-    for i in range(10):
-        print(f"{commit_list[i].get('sha')}: "
-              f"{commit_list[i].get('commit').get('author').get('name')}")
+    try:
+        repo_name = argv[1]
+        owner_name = argv[2]
+        url = f'https://api.github.com/repos/{owner_name}/{repo_name}/commits'
+        headers = {'Accept': 'application/vnd.github+json',
+                   'X-GitHub-Api-Version': '2022-11-28'}
+        res = requests.get(url, headers=headers)
+        commit_list = res.json()
+        for i in range(10):
+            print(f"{commit_list[i].get('sha')}: "
+                  f"{commit_list[i].get('commit').get('author').get('name')}")
+    except Exception:
+        pass
