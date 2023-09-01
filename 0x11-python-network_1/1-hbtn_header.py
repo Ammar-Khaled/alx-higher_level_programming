2361387b474bd7e1a7dd669bf96687df06d74189
Ammar-Khaled <ammar.khaled.github@gmail.com>
2023-09-01 09:36:08 +0000

    fix style

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
-"""This module takes in a URL, sends a request to the URL and displays the value of the X-Request-Id variable found in the header of the response."""
+"""
+This module takes in a URL,
+sends a request to the URL and
+displays the value of the 'X-Request-Id' variable
+found in the header of the response.
+"""
 
 if __name__ == '__main__':
     from urllib.request import urlopen
--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
-"""This module takes in a URL and an email, sends a POST request to the passed URL with the email as a parameter,
-and displays the body of the response (decoded in utf-8) with error handling."""
+"""
+This module takes in a URL and an email,
+sends a POST request to the passed URL with the email as a parameter,
+and displays the body of the response with HTTPError handling."""
 
 if __name__ == '__main__':
     from urllib.request import urlopen
