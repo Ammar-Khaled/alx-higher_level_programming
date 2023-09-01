22efad68ad6dc63f85b199d68ab29e7a5fd1f319
Ammar-Khaled <ammar.khaled.github@gmail.com>
2023-09-01 16:10:09 +0000

    fix

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -7,15 +7,13 @@ and uses the GitHub API to display the last recent 10 commits.
 if __name__ == '__main__':
     import requests
     from sys import argv
-    import json
     repo_name = argv[1]
     owner_name = argv[2]
     url = f'https://api.github.com/repos/{owner_name}/{repo_name}/commits'
     headers = {'Accept': 'application/vnd.github+json',
                'X-GitHub-Api-Version': '2022-11-28'}
     res = requests.get(url, headers=headers)
-    res_dict = json.loads(res.text)
+    commit_list = res.json()
     for i in range(10):
-        print(f"{res_dict.get(i).get('sha')}: \
-            {res_dict.get(i).get('commit').get('author').get('name')}")
-
+        print(f"{commit_list[i].get('sha')}: "
+              f"{commit_list[i].get('commit').get('author').get('name')}")
