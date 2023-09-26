6f28032f9feca1af96bf6844a7161fbf1e5318b9
Ammar-Khaled <ammar.khaled.github@gmail.com>
2023-09-26 16:45:26 +0000

    Did task5

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+const url = process.argv[2];
+const filePath = process.argv[3];
+const request = require('request');
+const fs = require('fs');
+
+
+request(url, (error, response) => {
+  if (error == null) {
+    fs.writeFile(filePath, response.body, 'utf8', (err) => {
+      if (err) {
+        console.error(err);
+      }
+    });
+  }
+});
