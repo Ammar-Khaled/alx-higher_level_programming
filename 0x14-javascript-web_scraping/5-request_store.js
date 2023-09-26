89e3947f50ca7fea56c8a8100f35868adde48106
Ammar-Khaled <ammar.khaled.github@gmail.com>
2023-09-26 16:46:23 +0000

    fix style

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -4,7 +4,6 @@ const filePath = process.argv[3];
 const request = require('request');
 const fs = require('fs');
 
-
 request(url, (error, response) => {
   if (error == null) {
     fs.writeFile(filePath, response.body, 'utf8', (err) => {
