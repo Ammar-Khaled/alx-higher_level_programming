25394f1cea69460b38732822bb3ff1542b948492
Ammar-Khaled <ammar.khaled.github@gmail.com>
2023-09-26 16:42:49 +0000

    [Fix]: printing zero when failed

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,21 +1,19 @@
 #!/usr/bin/node
 const request = require('request');
 const url = process.argv[2];
-const ourCharacter = 'https://swapi-api.alx-tools.com/api/people/18/';
+let count = 0;
 request(url, (error, response) => {
-  if (error) {
-    console.error(error);
-  } else {
+  if (error == null) {
     const films = JSON.parse(response.body).results;
-    let count = 0;
     for (let i = 0; i < films.length; i++) {
-      for (let j = 0; j < films[i].characters.length; j++) {
-        if (films[i].characters[j] === ourCharacter) {
+      const characters = films[i].characters;
+      for (let j = 0; j < characters.length; j++) {
+        if (characters[j].search('18') > 0) {
           count++;
         }
       }
     }
-    console.log(count);
   }
+  console.log(count);
 }
 );
