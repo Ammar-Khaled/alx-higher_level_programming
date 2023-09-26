364e8f82c0a79598df13660ec34df7d99f517114
Ammar-Khaled <ammar.khaled.github@gmail.com>
2023-09-26 15:02:29 +0000

    Used another approach

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,10 +1,21 @@
 #!/usr/bin/node
 const request = require('request');
-const url = 'https://swapi-api.alx-tools.com/api/people/18';
+const url = process.argv[2];
+const ourCharacter = 'https://swapi-api.alx-tools.com/api/people/18/';
 request(url, (error, response) => {
   if (error) {
     console.error(error);
   } else {
-    console.log(JSON.parse(response.body).films.length);
+    const films = JSON.parse(response.body).results;
+    let count = 0;
+    for (let i = 0; i < films.length; i++) {
+      for (let j = 0; j < films[i].characters.length; j++) {
+        if (films[i].characters[j] === ourCharacter) {
+          count++;
+        }
+      }
+    }
+    console.log(count);
   }
-});
+}
+);
